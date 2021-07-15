52791890d085fb29c4366bfca222ea6bc961b6c3
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-07-15 14:26:05 +0530

    fix:dynamic_array
    Signed-off-by: Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>

--- src/test.cpp
+++ src/test.cpp
@@ -32,37 +32,36 @@ int main()
     {
         std::cout << it << " ";
     }
+    custom::dynamic_array<std::shared_ptr<TestData>> temp_array;
 
-    custom::dynamic_array<TestData> temp_array;
-
-    temp_array.emplace_back(2, 3, 5);
-    temp_array.emplace_back(6, 7, 8);
-    temp_array.emplace_back(9, 10, 11);
-    temp_array.emplace_back(12, 13, 14);
-    temp_array.emplace_back(22, 23, 25);
-    temp_array.emplace_back(32, 33, 35);
-    temp_array.emplace_back(42, 43, 45);
-    temp_array.emplace_back(52, 53, 55);
-    temp_array.emplace_back(62, 63, 65);
+    temp_array.push_back(std::make_shared<TestData>(1, 3, 5));
+    temp_array.push_back(std::make_shared<TestData>(6, 7, 8));
+    temp_array.push_back(std::make_shared<TestData>(9, 10, 11));
+    temp_array.push_back(std::make_shared<TestData>(12, 13, 14));
+    temp_array.push_back(std::make_shared<TestData>(22, 23, 25));
+    temp_array.push_back(std::make_shared<TestData>(32, 33, 35));
+    temp_array.push_back(std::make_shared<TestData>(42, 43, 45));
+    temp_array.push_back(std::make_shared<TestData>(52, 53, 55));
+    temp_array.push_back(std::make_shared<TestData>(62, 63, 65));
 
     std::cout << "\n[] based for loop:\n";
 
     for (int i = 0; i < temp_array.size(); i++)
-        std::cout << temp_array[i].x << ", " << temp_array[i].y << ", " << temp_array[i].z  << "\n";
+        std::cout << temp_array[i]->x << ", " << temp_array[i]->y << ", " << temp_array[i]->z  << "\n";
 
     std::cout << "--------------------\n";
 
     std::cout << "\n Iterators based for loop:\n";
 
     for (auto it = temp_array.begin(); it != temp_array.end(); it++)
-        std::cout << it->x << ", " << it->y << ", " << it->z  << "\n";
+        std::cout << it->get()->x << ", " << it->get()->y << ", " << it->get()->z  << "\n";
     
     std::cout << "--------------------\n";
 
     std::cout << "\n Range based for loop:\n";
 
     for(auto& it : temp_array)
-        std::cout << it.x << ", " << it.y << ", " << it.z  << "\n";
+        std::cout << it->x << ", " << it->y << ", " << it->z  << "\n";
 
     std::cout << "\n";
 }
--- src/test_map.cpp
+++ src/test_map.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "hashmap.hpp"
+// #include "hashmap.hpp"
 
 int main()
 {
-    custom::hashmap<std::string, std::string> map;
-    map["hello"] = "world";
-    std::cout << map["Hello"] << "\n";
+    // custom::hashmap<std::string, std::string> map;
+    // map["hello"] = "world";
+    // std::cout << map["Hello"] << "\n";
 }
